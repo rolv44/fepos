da101c008edae956a0f22920a01b03dd0f203810
JohnVelaMacedo <jvela311297@gmail.com>
2019-08-08 23:28:19 -0500

    Terminando Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,20 +4,19 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card" style="background-color: transparent; border: none; margin-top: 15%">
-                {{-- <div class="card-header">{{ __('Iniciar Sesión') }}</div> --}}
+            <div class="card" style="background-color: transparent; border: none;">
 
-                <div class="card-body">
+                <div class="card-body" style="margin: 10px auto; width: 80%">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
 
-                            <div class="col-md-10">
+                            <div class="col-12 col-md-10">
                                 <input id="user" type="text" class="form-control @error('user') is-invalid @enderror" 
                                     name="user" value="{{ old('user') }}" required autocomplete="user" autofocus placeholder="Usuario" 
-                                    style="border-radius: 20px; width: 300px; background-color: rgba(255,255,255,.1); color: white!important; 
-                                    border-color: transparent">
+                                    style="border-radius: 20px; background-color: rgba(255,255,255,.1); color: white!important; 
+                                    border-color: transparent;">
 
                                 @error('user')
                                     <span class="invalid-feedback" role="alert">
@@ -29,10 +28,11 @@
 
                         <div class="form-group row">
 
-                            <div class="col-md-10 text-center">
+                            <div class="col-12 col-md-10">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" 
                                     name="password" required autocomplete="current-password" placeholder="Contraseña" 
-                                    style="border-radius: 20px; width: 300px;">
+                                    style="border-radius: 20px; background-color: rgba(255,255,255,.1); color: white!important; 
+                                    border-color: transparent;">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -43,8 +43,8 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-12 text-center">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-12 col-md-10 text-center">
+                                <button type="submit" class="btn btn-primary d-block" style="width: 100%; border-radius: 20px">
                                     {{ __('Iniciar Sesión') }}
                                 </button>
 
